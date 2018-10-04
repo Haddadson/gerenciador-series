782141f7efff4b3c86f52e518f6b4389f9ad6a77
Gabriel Haddad Campos <dti250@localiza.com>
2018-10-04 01:30:58 -0300

    Adiciona gravação de favoritos em arquivo

--- src/business/OperacoesLista.java
+++ src/business/OperacoesLista.java
@@ -16,16 +16,18 @@ public class OperacoesLista {
 
 	public OperacoesLista() {
 		this.lista = new Lista();
+		this.lista = lerArquivoSeries();
 		this.listaFavoritos = new Lista();
+		this.listaFavoritos = lerArquivoFavoritos();
 		OperacoesLista.listaSugestoesAleatorias = new Lista();
-		this.lista = lerArquivo();
+		
 	}
 
 	public Lista getLista() {
 		return lista;
 	}
 
-	public Lista lerArquivo() {
+	public Lista lerArquivoSeries() {
 
 		try {
 			BufferedReader in = new BufferedReader(
@@ -44,17 +46,35 @@ public class OperacoesLista {
 		}
 		return lista;
 	}
+	
+	public Lista lerArquivoFavoritos() {
+		try {
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(new FileInputStream(CAMINHO_ARQUIVO_FAVORITOS), "ISO-8859-1"));
+			String linha = in.readLine();
+			while (linha != null) {
+				String atributos[] = linha.split(";");
+				this.listaFavoritos.inserirFim(preencherFavorito(atributos));
+				linha = in.readLine();
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Arquivo \"" + CAMINHO_ARQUIVO_FAVORITOS + "\"  não existe.");
+		} catch (IOException e) {
+			System.out.println("Erro na leitura do arquivo " + CAMINHO_ARQUIVO_FAVORITOS + ".");
+		}
+		return listaFavoritos;
+	}
 
 	public void gravarFavoritos() {
 		try {
-			
-			FileWriter arq = new FileWriter(CAMINHO_ARQUIVO_FAVORITOS);
-		    PrintWriter gravarArq = new PrintWriter(arq);
-		    gravarArq.printf(buscarTodasSeriesFavoritasFormatadas());
+			BufferedWriter buffWrite = new BufferedWriter(new FileWriter(CAMINHO_ARQUIVO_FAVORITOS));
+			buffWrite.write(buscarTodasSeriesFavoritasFormatadas());
+		    buffWrite.close();
 		} catch (FileNotFoundException e) {
-			System.out.println("Arquivo \"" + CAMINHO_ARQUIVO_SERIES + "\"  não existe.");
+			System.out.println("Arquivo \"" + CAMINHO_ARQUIVO_FAVORITOS + "\"  não existe.");
 		} catch (IOException e) {
-			System.out.println("Erro na leitura do arquivo " + CAMINHO_ARQUIVO_SERIES + ".");
+			System.out.println("Erro na leitura do arquivo " + CAMINHO_ARQUIVO_FAVORITOS + ".");
 		}
 	}
 
@@ -74,10 +94,31 @@ public class OperacoesLista {
 		}
 		return serie;
 	}
+	
+	public Serie preencherFavorito(String atributos[]) {
+		Serie serie = new Serie();
+		if (atributos.length == 10) {
+			serie.setId(Integer.parseInt(atributos[0]));
+			serie.setNome(atributos[1]);
+			serie.setTipo(atributos[2]);
+			serie.setDuracao(atributos[3]);
+			serie.setPais(atributos[4]);
+			serie.setIdioma(atributos[5]);
+			serie.setEmissora(atributos[6]);
+			serie.setTransmissao(atributos[7]);
+			serie.setNumTemporadas(atributos[8]);
+			serie.setNumEpisodios(atributos[9]);
+		}
+		return serie;
+	}
+	
+	
 
 	public boolean favoritarSerie(Serie serie) {
 		if (listaFavoritos.pesquisarPorIdSerie(serie.getId()) == null) {
 			listaFavoritos.inserirFim(serie);
+			System.out.println(listaFavoritos.formatarListaParaArquivo());
+			gravarFavoritos();
 			return true;
 		}
 		return false;
@@ -109,7 +150,9 @@ public class OperacoesLista {
 
 	public Serie removerFavoritoPorId(int id) {
 		try {
-			return listaFavoritos.removerPorIdSerie(id);
+			Serie serie = listaFavoritos.removerPorIdSerie(id);
+			gravarFavoritos();
+			return serie;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
